3282ce6a66cfba16746b36f9ab7f5a8f64490fb5
Abraham Shimelis <abrahamshimelis@gmail.com>
2023-01-19 10:32:12 +0000

    debug task 0

--- monty.h
+++ monty.h
@@ -46,10 +46,8 @@ instruct_func get_opcode_func(char *str);
 void read_file(char *filename, stack_t **stack);
 void _pall(stack_t **stack, unsigned int line_number);
 void _push(stack_t **stack, unsigned int line_number);
-stack_t *add_dnodeint_end(stack_t **head, const int n);
 stack_t *add_dnodeint(stack_t **head, const int n);
 void free_dlistint(stack_t *head);
-int delete_dnodeint_at_index(stack_t **head, unsigned int index);
 void error_exit(stack_t **stack);
 int isnumber(char *str);
 #endif
